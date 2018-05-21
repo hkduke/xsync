f03a9a64aa51e54a2bb6b883f2a3ecd6e80a951f
pepstack <350137278@qq.com>
2018-05-22 00:55:09 +0800

    ok

--- src/server/server_api.c
+++ src/server/server_api.c
@@ -282,7 +282,7 @@ extern XS_RESULT XS_server_create (xs_appopts_t *opts, XS_server *outServer)
     int TIMEOUTMS = opts->timeout_ms;
 
     // TODO:
-    const char dbpool_url[] = "mysql://211.152.44.82/xsync?user=root&password=Abc1234";
+    const char dbpool_url[] = "mysql://localhost/xsyncdb?user=xsync&password=p@ssW0rd!";
 
     LOGGER_DEBUG("zdbpool_init: (url=%s, maxsize=%d)", dbpool_url, ZDBPOOL_MAX_SIZE);
 
