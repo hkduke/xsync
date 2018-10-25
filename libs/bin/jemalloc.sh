5194ba9c72046a5c5a2ce82d4bb59ffe2074b314
pepstack <350137278@qq.com>
2018-10-25 17:37:38 +0800

    remove libs

--- libs/bin/jemalloc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-prefix=/root/Workspace/github.com/pepstack/xsync/libs
-exec_prefix=/root/Workspace/github.com/pepstack/xsync/libs
-libdir=${exec_prefix}/lib
-
-LD_PRELOAD=${libdir}/libjemalloc.so.2
-export LD_PRELOAD
-exec "$@"
