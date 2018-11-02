6a6266fbf3acdc6ac5202a118bf4183f9d2ba309
pepstack <350137278@qq.com>
2018-11-02 14:34:53 +0800

    install daemontools

--- deps/install-daemontools.sh
+++ deps/install-daemontools.sh
@@ -30,34 +30,40 @@ set -o nounset
 # Treat any error as exit
 set -o errexit
 
+# 安装路径
+INSTALLPREFIX="/usr/local/lib"
+
 ###########################################################
+# 安装 daemontools 到 $INSTALLPREFIX
+
 function install_daemontools()
 {
-    local prefix="/usr/local"
-
     local pkgname="daemontools-0.76.tar.gz"
 
-    echoinfo "installing: $pkgname at: $prefix/daemontools"
+    echoinfo "installing: $pkgname at: $INSTALLPREFIX/daemontools"
 
     cd ${_cdir}
 
     tar -zxf "${_cdir}/$pkgname"
-    rm -rf "$prefix/daemontools-0.76"
-    mv "${_cdir}/daemontools-0.76" "$prefix/"
-    cd "$prefix/daemontools-0.76"
+    rm -rf "$INSTALLPREFIX/daemontools-0.76"
+    mv "${_cdir}/daemontools-0.76" "$INSTALLPREFIX/"
+    cd "$INSTALLPREFIX/daemontools-0.76"
     sh ./package/install
 
     cd ${_cdir}
 
     if [ -d "/etc/systemd/system/" ]; then
-        # el7 and later
+        echowarn "/etc/inittab is no longer used when using systemd"
 
+        sed -i '/svscanboot/d' /etc/inittab
+
+        # for el7 and later
         if [ -f "/etc/systemd/system/daemontools.service" ]; then
             rm -rf "/etc/systemd/system/daemontools.service.backup"
             mv "/etc/systemd/system/daemontools.service" "/etc/systemd/system/daemontools.service.backup"
         fi
 
-        copy "${_cdir}/daemontools.service" "/etc/systemd/system/"
+        cp "${_cdir}/daemontools.service" "/etc/systemd/system/"
     fi
 }
 
@@ -79,7 +85,7 @@ function start_daemontools()
             else
                 echoinfo "starting command: '/command/svscanboot &'"
 
-                csh -cf '/command/svscanboot &'
+                /command/svscanboot &
             fi
 
             sleep 3
@@ -102,13 +108,17 @@ echoinfo "refer: [1] http://cr.yp.to/daemontools.html"
 echoinfo "       [2] https://blog.csdn.net/superbfly/article/details/52877954"
 
 if [[ ! -d "/service" && ! -d "/command" ]]; then
-    echoinfo "install daemontools at: /usr/local/daemontools"
+    echoinfo "install daemontools at: $INSTALLPREFIX/daemontools"
 
     install_daemontools;
+    
+    start_daemontools;
 elif [[ -d "/service" && -d "/command" ]]; then
     dmrootlk=$(dirname $(dirname $(readlink "/command/supervise")))
 
     echowarn "daemontools already installed at: $dmrootlk -> $(readlink $dmrootlk)"
+    
+    start_daemontools;
 else
     if [[ -d "/service" ]]; then
         echowarn "daemontools found: /service"
@@ -122,5 +132,3 @@ else
 
     exit -1
 fi
-
-start_daemontools;
