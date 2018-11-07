02b051e368d056a9cbf06781a44fdd4faece1ba2
pepstack <350137278@qq.com>
2018-11-07 18:55:23 +0800

    test.lua add safe box

--- bin/test.lua
+++ bin/test.lua
@@ -6,96 +6,122 @@ package.cpath = package.cpath .. ";./?.so;../lib/lua/5.3/cjson.so;../libs/lib/lu
 -- 必须引入安全调用模块: trycall.lua
 require("__trycall")
 
----[[
--- 加载 cjson 模块:
 
-local js = require("cjson")
-
+--[[
+  intab = {
+    jsonfile = "/root/Downloads/shop01-rdb_6101.rdb.json",
+    jsonoutfile = "/root/Downloads/shop01-rdb_6101.rdb.json.out"
+  }
 --]]
 
-local BUFSIZE = 8192
+function dump_qiuqiu_jsonfile(intab)
+    -- 返回值
+    local outab = {
+        result = "ERROR"
+    }
 
-local jsonfile = "/root/Downloads/shop01-rdb_6101.rdb.json"
-local jsonoutfile = "/root/Downloads/shop01-rdb_6101.rdb.json.out"
+    -- 加载 cjson 模块:
+    local js = require("cjson")
 
-local tstart = os.time()
+    local BUFSIZE = 8192
 
-print(string.format("start time : %d", tstart))
+    local jsonfile = intab.jsonfile
+    local jsonoutfile = intab.jsonoutfile
 
-print(string.format("input file : %s", jsonfile))
-print(string.format("output file: %s", jsonoutfile))
+    local tstart = os.time()
+
+    print(string.format("start time : %d", tstart))
+
+    print(string.format("input file : %s", jsonfile))
+    print(string.format("output file: %s", jsonoutfile))
 
 
--- 打开输入文件
-local fin = io.input(jsonfile)
+    -- 打开输入文件
+    local fin = io.input(jsonfile)
 
--- 输出文件会被覆盖!!
-local fout = io.open(jsonoutfile, "w")
+    -- 输出文件会被覆盖!!
+    local fout = io.open(jsonoutfile, "w")
 
--- 行计数
-local lc = 0
-local lw = 0
+    -- 行计数
+    local lc = 0
+    local lw = 0
 
-while true do
-    local lines, rest = fin:read(BUFSIZE, "*line")
-    if not lines then
-        break
-    end
+    while true do
+        local lines, rest = fin:read(BUFSIZE, "*line")
+        if not lines then
+            break
+        end
 
-    if rest then
-        lines = lines .. rest .. "\n"
-    end
+        if rest then
+            lines = lines .. rest .. "\n"
+        end
 
-    for line in lines:gmatch("[^\r\n]+") do
-        -- 计算输入行数    
-        lc = lc + 1
-        
-        jstab = js.decode(line)
+        for line in lines:gmatch("[^\r\n]+") do
+            -- 计算输入行数    
+            lc = lc + 1
+            
+            jstab = js.decode(line)
 
-        local genid = jstab.gen_id
-        local rekey = jstab.redis_key
+            local genid = jstab.gen_id
+            local rekey = jstab.redis_key
 
-        local rkval = jstab[rekey]
-        
-        if rkval ~= nil then
+            local rkval = jstab[rekey]
+            
+            if rkval ~= nil then
 
-            if type(rkval) == "string" then
-                jsobjects = js.decode(rkval)
+                if type(rkval) == "string" then
+                    jsobjects = js.decode(rkval)
 
-                for k, v in pairs(jsobjects) do
-                    message = string.format("%s|%s|%s\n", genid, v.obj, v.t)
+                    for k, v in pairs(jsobjects) do
+                        message = string.format("%s|%s|%s\n", genid, v.obj, v.t)
 
-                    fout:write(message)
+                        fout:write(message)
 
-                    -- 计算输出行数
-                    lw = lw + 1
+                        -- 计算输出行数
+                        lw = lw + 1
+                    end
+                else
+                    for k, v in pairs(rkval) do
+                        message = string.format("%s|%s|%s", genid, v.obj, v.t)
+
+                        fout:write(message)
+
+                        -- 计算输出行数
+                        lw = lw + 1
+                    end
                 end
-            else
-                for k, v in pairs(rkval) do
-                    message = string.format("%s|%s|%s", genid, v.obj, v.t)
 
-                    fout:write(message)
-
-                    -- 计算输出行数
-                    lw = lw + 1
-                end
             end
 
         end
-
+        
+        fout:flush()
     end
-    
-    fout:flush()
+
+    -- 关闭文件
+    fin:close()
+    fout:close()
+
+    local tend = os.time()
+
+    print(string.format("total input  lines: %d", lc))
+    print(string.format("total output lines: %d", lw))
+    print(string.format("elapsed seconds   : %d", tend - tstart))
+    print(string.format("input lines speed : %f line per second", lc / (tend - tstart)))
+    print(string.format("output lines speed: %f line per second", lw / (tend - tstart)))
+
+    -- 返回值
+    outab.result = "SUCCESS"
+    return outab
 end
 
--- 关闭文件
-fin:close()
-fout:close()
 
-local tend = os.time()
+-- main function
+--  
+local out = __trycall("dump_qiuqiu_jsonfile", {
+        jsonfile = "/root/Downloads/shop01-rdb_6101.rdb.json",
+        jsonoutfile = "/root/Downloads/shop01-rdb_6101.rdb.json.out"
+    })
 
-print(string.format("total input  lines: %d", lc))
-print(string.format("total output lines: %d", lw))
-print(string.format("elapsed seconds   : %d", tend - tstart))
-print(string.format("input lines speed : %f line per second", lc / (tend - tstart)))
-print(string.format("output lines speed: %f line per second", lw / (tend - tstart)))
+print("result=" .. out.result)
+print("exception=" .. out.exception)
